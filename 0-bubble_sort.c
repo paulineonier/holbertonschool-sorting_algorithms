be38e411dbbd0e78e93276cf4dad15f4660ea336
paulineonier <pauline.onier@outlook.fr>
2024-04-11 14:17:34 +0000

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - orts an array of integers in ascending order using Bubble sort
+ * @array: Array to be sorted
+ * @size: Size of array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t i, j;
+
+	if (array == NULL || size == 0)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
