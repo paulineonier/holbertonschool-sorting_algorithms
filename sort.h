92727f53cd9827aa9b4c1145354338ccb0941eda
paulineonier <pauline.onier@outlook.fr>
2024-04-11 11:40:15 +0000

    main

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
+
+#include <stddef.h>
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif
