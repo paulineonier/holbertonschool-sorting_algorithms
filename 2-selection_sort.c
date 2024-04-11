fc09e2c02be4222d88934ce6f37ae15e74dc4b6a
paulineonier <pauline.onier@outlook.fr>
2024-04-11 15:21:35 +0000

    task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+/**
+ * _swap - swap two numbers
+ * @a: integer
+ * @b: integer
+ **/
+
+void _swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - sorts array integers in ascending order with Selection sort
+ * @array: array to be sort
+ * @size: size of array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	unsigned int i, j, min;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[min] > array[j])
+				min = j;
+		}
+		if (min != i)
+		{
+			_swap(&array[i], &array[min]);
+			print_array(array, size);
+		}
+	}
+
+}
